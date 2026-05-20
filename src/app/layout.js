336395313c0db7e1a5f4ab1460909d40c0b690f2
Ahmad Said <mail.saydorrahman@gmail.com>
2026-05-20 22:15:12 +0600

    style: integrate Inter font as system default for minimalist typography

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,22 +2,19 @@ import dns  from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 import ThemeProvider from "@/components/providers/ThemeProvider";
 
-const geistSans = Geist({
+const interFont = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -28,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${interFont.className}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
       
